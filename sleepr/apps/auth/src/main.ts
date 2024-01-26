20e1307edae7186697b10e93843a50213557623a
RobinB87 <robinbette@hotmail.com>
2024-01-26 08:52:21 +0000

    hook up the microservices

--- sleepr/apps/auth/src/main.ts
+++ sleepr/apps/auth/src/main.ts
@@ -4,12 +4,16 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
 import { AuthModule } from './auth.module';
 import * as cookieParser from 'cookie-parser';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.connectMicroservice({ transport: Transport.TCP });
   app.use(cookieParser()); // middleware (all requests with cookie will execute through this)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+
+  await app.startAllMicroservices(); // start our ms up over TCP and listen for events and requests
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
