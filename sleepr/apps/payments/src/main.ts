f6c2c00aa04b6bb6a6131949e64f21b07ffb942f
RobinB87 <robinbette@hotmail.com>
2024-01-26 10:27:04 +0000

    set up pino logger

--- sleepr/apps/payments/src/main.ts
+++ sleepr/apps/payments/src/main.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
+import { Logger } from 'nestjs-pino';
 import { PaymentsModule } from './payments.module';
 
 async function bootstrap() {
@@ -14,6 +15,8 @@ async function bootstrap() {
       port: configService.get('PORT'),
     },
   });
+
+  app.useLogger(app.get(Logger));
 
   await app.startAllMicroservices();
 }
