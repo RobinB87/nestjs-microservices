0a173a28498c0f50b94ed21466c3d2d89f7b48e9
RobinB87 <robinbette@hotmail.com>
2024-01-26 09:48:07 +0000

    * Create new common auth guard that all MS can use * Make use of a common UserDto

---
+++ sleepr/libs/common/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { Observable, map, tap } from 'rxjs';
+import { AUTH_SERVICE } from '../constants/services';
+import { UserDto } from '../dto';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  // The client proxy allows us to communicate between MS
+  constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
+    if (!jwt) return false;
+
+    return this.authClient
+      .send<UserDto>('authenticate', {
+        Authentication: jwt,
+      })
+      .pipe(
+        tap((res) => {
+          // add current user on request via the response, which is actually the user itself
+          context.switchToHttp().getRequest().user = res;
+        }),
+        // return true if we have a succesful response from the auth MS
+        map(() => true),
+      );
+  }
+}
