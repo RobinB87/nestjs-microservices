c8d65699530304e94cc23cbbd4d205af27d8b830
RobinB87 <robinbette@hotmail.com>
2024-01-26 10:04:40 +0000

    * fix issue with 'connection closed' by specifying host and port to listen on * also move these ports to .env

--- sleepr/apps/auth/src/main.ts
+++ sleepr/apps/auth/src/main.ts
@@ -8,14 +8,22 @@ import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  app.connectMicroservice({ transport: Transport.TCP });
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0', // bind to all interfaces on the host
+      port: configService.get('TCP_PORT'),
+    },
+  });
+
   app.use(cookieParser()); // middleware (all requests with cookie will execute through this)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
 
   await app.startAllMicroservices(); // start our ms up over TCP and listen for events and requests
 
-  const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'));
+  await app.listen(configService.get('HTTP_PORT'));
 }
 bootstrap();
