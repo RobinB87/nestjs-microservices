1e618c28871c07cb4c9973afe680c0982803bfa2
RobinB87 <robinbette@hotmail.com>
2024-01-26 10:25:53 +0000

    * make payments a standalone ms * set up configservice

--- sleepr/apps/payments/src/payments.module.ts
+++ sleepr/apps/payments/src/payments.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
