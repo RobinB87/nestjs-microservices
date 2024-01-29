2fc245623e4ea8230750665157e0c683e4d416c2
RobinB87 <robinbette@hotmail.com>
2024-01-29 16:20:28 +0000

    fix validation for charge

--- sleepr/libs/common/src/dto/create-charge.dto.ts
+++ sleepr/libs/common/src/dto/create-charge.dto.ts
@@ -1,4 +1,5 @@
 import { CardDto } from '@app/common';
+import { Type } from 'class-transformer';
 import {
   IsDefined,
   IsNotEmptyObject,
@@ -10,6 +11,7 @@ export class CreateChargeDto {
   @IsDefined()
   @IsNotEmptyObject()
   @ValidateNested()
+  @Type(() => CardDto)
   card: CardDto;
 
   @IsNumber()
