a82c2ef7d3ca3d7f8063d204632b3698c2bed5ab
RobinB87 <robinbette@hotmail.com>
2024-01-29 16:29:30 +0000

    fix issue with test data for stripe

--- sleepr/apps/payments/src/payments.service.ts
+++ sleepr/apps/payments/src/payments.service.ts
@@ -14,18 +14,13 @@ export class PaymentsService {
 
   constructor(private readonly configService: ConfigService) {}
 
-  async createCharge({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    });
-
+  async createCharge({ amount }: CreateChargeDto) {
     return await this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id,
       amount: amount * 100,
       confirm: true,
-      payment_method_types: ['card'],
       currency: 'eur',
+      payment_method: 'pm_card_visa',
+      return_url: 'https://www.google.com',
     });
   }
 }
