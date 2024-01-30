c707f6b27db983e86d3ff211870569923ea5922e
RobinB87 <robinbette@hotmail.com>
2024-01-30 09:57:43 +0000

    add todo

--- sleepr/apps/notifications/src/notifications.service.ts
+++ sleepr/apps/notifications/src/notifications.service.ts
@@ -4,6 +4,7 @@ import { NotifyEmailDto } from '../dto/notify-email.dto';
 @Injectable()
 export class NotificationsService {
   notifyEmail({ email }: NotifyEmailDto) {
+    // TODO: add nodemail and gmail stuff at some point.
     console.log(email);
   }
 }
