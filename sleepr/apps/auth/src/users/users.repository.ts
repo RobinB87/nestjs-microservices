47fe7a7e942c30dd7e0710396e9aed5077589d73
RobinB87 <robinbette@hotmail.com>
2024-01-30 11:01:26 +0000

    add @Injectable()

--- sleepr/apps/auth/src/users/users.repository.ts
+++ sleepr/apps/auth/src/users/users.repository.ts
@@ -1,8 +1,9 @@
 import { AbstractRepository, UserDocument } from '@app/common';
-import { Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+@Injectable()
 export class UsersRepository extends AbstractRepository<UserDocument> {
   protected readonly logger = new Logger(UsersRepository.name);
 
